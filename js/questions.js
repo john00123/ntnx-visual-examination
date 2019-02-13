f4d3d58c59db65e5431aa7c736569939ba00804b
John Torres <john.torres.hurtado@gmail.com>
2019-02-12 21:24:50 -0800

    unforgiving 4 questions

--- js/questions.js
+++ js/questions.js
@@ -1,32 +1,32 @@
 let qData =[
   { q_num: 1,
     images:[
-      {pic: 'https://airbnb.design/wp-content/uploads/2018/03/AB-MAR18-Speaking-Of-Final-Yellow.jpg', value: 'a'},
-      {pic: 'https://airbnb.design/wp-content/uploads/2018/03/AB-MAR18-Speaking-Of-Final-Blue.jpg', value: 'b'}
+      {pic: '../img/1_A.jpg', value: 'a'},
+      {pic: '../img/1_B.jpg', value: 'b'}
     ],
     comment:'',
     answer :''
   },
   { q_num: 2,
     images:[
-      {pic: 'https://airbnb.design/wp-content/uploads/2018/03/AB-MAR18-Graceful-Exits-Final.png', value: 'a'},
-      {pic: 'https://airbnb.design/wp-content/uploads/2018/03/AB-MAR18-Graceful-Exits-Final.png', value: 'b'}
+      {pic: '../img/2_A.jpg', value: 'a'},
+      {pic: '../img/2_B.jpg', value: 'b'}
     ],
     comment:'',
     answer :''
   },
   { q_num: 3,
     images:[
-      {pic: 'https://airbnb.design/wp-content/uploads/2018/03/Airbnb-for-Everyone.png', value: 'a'},
-      {pic: 'https://airbnb.design/wp-content/uploads/2018/04/AB-APR18-Leveraging-Hacks-Final.jpg', value: 'b'}
+      {pic: '../img/3_A.jpg', value: 'a'},
+      {pic: '../img/3_B.jpg', value: 'b'}
     ],
     comment:'',
     answer :''
   },
   { q_num: 4,
     images:[
-      {pic: 'https://airbnb.design/wp-content/uploads/2017/11/working-towards-inclusion-header.gif', value: 'a'},
-      {pic: 'https://airbnb.design/wp-content/uploads/2017/11/working-towards-inclusion-header.gif', value: 'b'}
+      {pic: '../img/4_A.jpg', value: 'a'},
+      {pic: '../img/4_B.jpg', value: 'b'}
     ],
     comment:'',
     answer :''
